8c2a103b8650fafeb13ae454631c1aca750d7323
dembol23 <wdembny.2005@gmail.com>
2024-09-07 21:47:19 +0200

    Inital commit

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 const playButton = document.getElementById("play-button")
 playButton.addEventListener("click", function () {
-    window.location.href = "/game.html";
+    window.location.pathname = "/game.html";
 })
